c07bd47673d3b89495a16b378f8112dea333b13f
Asmit Kumar <asmit2864@gmail.com>
2025-05-01 15:43:58 +0530

    Add files via upload

---
+++ ca1/ca1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ca1
+//
+//  Created by Student on 17/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var lblemail: UILabel!
+    
+    @IBOutlet var lblpwd: UIView!
+    
+    @IBOutlet weak var txtemail: UITextField!
+    
+    @IBOutlet weak var txtpwd: UITextField!
+    
+    
+    @IBAction func btnlogin(_ sender: Any) {
+    }
+    
+    @IBAction func btnsup(_ sender: Any) {
+    }
+    
+    
+}
+
